9c737f2adc7ab7af45411c78f26892ef1e672b74
JM-Sheehan <jamie.michael.sheehan@gmail.com>
2020-12-15 11:18:17 +0000

    Full review page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage';
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";  // CHANGED
 import FavoriteMoviesPage from "./pages/favouriteMoviesPage";       // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
+
 
 const App = () => {
   return (
@@ -27,6 +29,7 @@ const App = () => {
             <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
             <Route path="/movies/:id" component={MoviePage} />
             <Route path="/" component={HomePage} />
+            <Route path="/reviews/:id" component={MovieReviewPage} />
             <Redirect from="*" to="/" />
           </Switch>
         </div>
--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,54 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import MovieDetails from "../components/movieDetails";
 import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from '../api/tmdb-api'
+import {getMovie} from '../api/tmdb-api'
 import { Link, Route, withRouter } from "react-router-dom"
 import MovieReviews from "../components/movieReviews"
 
 const MoviePage = props => {
-    const { id } = props.match.params;
-    const [movie, setMovie] = useState(null);
-    useEffect(() => {
-        getMovie(id).then(movie => {
-            setMovie(movie);
-        });
-    }, [id]);
-    return (
-        <>
-            {movie ? (
-                <>
-                    <PageTemplate movie={movie}>
-                        <MovieDetails movie={movie} />
-                    </PageTemplate>
-                    <div className="row">
-                        <div className="col-12 ">
-                            {!props.history.location.pathname.endsWith("/reviews") ? (
-                                <Link
-                                    className="btn btn-primary btn-block active"
-                                    to={`/movies/${id}/reviews`}
-                                >
-                                    Show Reviews (Extracts)
-                                </Link>
-                            ) : (
-                                    <Link
-                                        className="btn btn-primary btn-block active"
-                                        to={`/movies/${id}`}
-                                    >
-                                        Hide Reviews
-                                    </Link>
-                                )}
-                        </div>
-                    </div>
-                    <Route
-                        path={`/movies/:id/reviews`}
-                        render={props => <MovieReviews movie={movie} {...props} />}
-                    />
-                </>
+  const { id } = props.match.params;
+  const [movie, setMovie] = useState(null);
+  useEffect(() => {
+    getMovie(id).then(movie => {
+      setMovie(movie);
+    });
+  }, [id]);
+  return (
+    <>
+    {movie ? (
+      <>
+        <PageTemplate movie={movie}>
+          <MovieDetails movie={movie} />
+        </PageTemplate>
+        <div className="row">
+          <div className="col-12 ">
+            {!props.history.location.pathname.endsWith("/reviews") ? (
+              <Link
+                className="btn btn-primary btn-block active"
+                to={`/movies/${id}/reviews`}
+              >
+                Show Reviews (Extracts)
+              </Link>
             ) : (
-                    <p>Waiting for movie details</p>
-                )}
-        </>
-    );
+              <Link
+                className="btn btn-primary btn-block active"
+                to={`/movies/${id}`}
+              >
+                Hide Reviews 
+              </Link>
+            )}
+          </div>
+        </div>
+        <Route
+          path={`/movies/:id/reviews`}
+          render={props => <MovieReviews movie={movie} {...props} />}
+        />
+      </>
+    ) : (
+      <p>Waiting for movie details</p>
+    )}
+  </>
+  );
 };
 
 export default withRouter(MoviePage);
