c79d6a1cb5816a70f9c8a846db82f0a30d84d5c7
JM-Sheehan <jamie.michael.sheehan@gmail.com>
2020-12-16 12:51:37 +0000

    Add review supported

--- src/index.js
+++ src/index.js
@@ -9,22 +9,24 @@ import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
 import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="jumbotron">
-        <SiteHeader /> 
+        <SiteHeader />
         <div className="container-fluid">
           <MoviesContextProvider>
             <GenresContextProvider>    {/* NEW */}
               <Switch>
-            <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-            <Route path="/movies/:id" component={MoviePage} />
-            <Route path="/" component={HomePage} />
-            <Route path="/reviews/:id" component={MovieReviewPage} />
-            <Redirect from="*" to="/" />
-            </Switch>
+                <Route exact path="/reviews/form" component={AddMovieReviewPage} />
+                <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                <Route path="/movies/:id" component={MoviePage} />
+                <Route path="/" component={HomePage} />
+                <Route path="/reviews/:id" component={MovieReviewPage} />
+                <Redirect from="*" to="/" />
+              </Switch>
             </GenresContextProvider>    {/* NEW */}
           </MoviesContextProvider>
         </div>
